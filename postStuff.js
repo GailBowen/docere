14edb3c784e6c4c24fedc63bf593b82cce770ac4
Virgil <gailbowen1@gmail.com>
2018-05-01 13:45:19 +0100

    onsubmit needs 'return'

--- postStuff.js
+++ postStuff.js
@@ -1,3 +1,11 @@
 function testValidationFailed() {
     
+    if (document.getElementById("cardNumber").value.length < 2)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
