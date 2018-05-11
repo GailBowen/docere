5f6c1b87365e0aeac21dd4b569d6e2ba017a5f80
Virgil <gailbowen1@gmail.com>
2018-05-11 17:45:59 +0100

    arrays

---
+++ array.js
@@ -0,0 +1,15 @@
+function OptionOne()
+{
+    var myArray = ["One hundred", 100];
+
+    alert(myArray[0]);
+    alert(myArray[1]);
+}
+
+function OptionTwo()
+{
+    var myArray = ["One hundred", 100];
+
+    alert(myArray[0]);
+    alert(myArray[1]);
+}
