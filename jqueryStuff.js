c59b80b525c70991babbd82e40bbee258897ceb0
Gail Bowen <gailbowen1@gmail.com>
2018-05-22 22:15:39 +0100

    jquery background-color

--- jqueryStuff.js
+++ jqueryStuff.js
@@ -1,6 +1,10 @@
 function doStuff()
 {
   console.log($("#txtName").val());
-  
-  
 }
+
+//$("#txtName").css("background", "purple");
+
+$("table td input").focus(function() 
+{$(this).parent().parent().css("background-color", "purple");});
+
